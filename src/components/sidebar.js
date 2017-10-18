e19cc142cbe19257be8e46df38123e607b8f01d2
Matthew Taggart <matthew.taggart@cardcash.com>
2017-10-18 18:56:57 -0400

    Link to different routes in sidebar.

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SideBar = () => (
   <aside className="menu column is-one-quarter">
     <p className="menu-label">Browse</p>
     <ul className="menu-list">
-      <li><a>Characters</a></li>
-      <li><a>Comics</a></li>
-      <li><a>Creators</a></li>
+      <li><Link to="/">Characters</Link></li>
+      <li><Link to="/comics">Comics</Link></li>
+      <li><Link to="/creators">Creators</Link></li>
     </ul>
   </aside>
 );
