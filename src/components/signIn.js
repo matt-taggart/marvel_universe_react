049a51528bd177a279905bc902e08f2a675241e6
Matthew Taggart <matthew.taggart@cardcash.com>
2017-10-28 00:53:09 -0400

    Style contact form and do initial setup w/ redux store.

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,20 +1,55 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+const renderField = ({ input, type, placeholder, className, meta: { error } }) => (
+  <input {...input} className={className} type={type} placeholder={placeholder}></input> 
+);
 
 const SignIn = () => (
-  <div>
-    <div className="field">
-      <label className="label">Email</label>
-      <div className="control">
-        <input className="input is-medium" type="text" placeholder="Email" />
+  <div className="columns is-centered">
+    <div className="column is-two-thirds">
+      <div className="field">
+        <p className="control has-icons-left has-icons-right">
+          <Field
+            name="email"
+            className="input is-medium"
+            type="text"
+            placeholder="Email"
+            component={renderField}
+          />
+          <span className="icon is-small is-left">
+            <i className="fa fa-envelope"></i>
+          </span>
+          <span className="icon is-small is-right">
+            <i className="fa fa-check"></i>
+          </span>
+        </p>
       </div>
-    </div>
-    <div className="field">
-      <label className="label">Password</label>
-      <div className="control">
-        <input className="input is-medium" type="password" placeholder="Password" />
+      <div className="field">
+        <p className="control has-icons-left">
+          <Field
+            name="password"
+            className="input is-medium"
+            type="password"
+            placeholder="Password"
+            component={renderField}
+          />
+          <span className="icon is-small is-left">
+            <i className="fa fa-lock"></i>
+          </span>
+        </p>
+      </div>
+      <div className="field">
+        <p className="control">
+          <button className="button is-dark is-medium">
+            Sign In
+          </button>
+        </p>
       </div>
     </div>
   </div>
 );
 
-export default SignIn;
+export default reduxForm({
+  form: 'signIn',
+})(SignIn);
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import characters from './reducers/characters';
 import comics from './reducers/comics';
 import creators from './reducers/creators';
@@ -11,4 +12,5 @@ export default combineReducers({
   creators,
   events,
   display,
+  form: formReducer,
 });
