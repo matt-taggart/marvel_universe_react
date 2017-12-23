5cc1fe238b8c36124cabdf686546720c98861697
Matthew Taggart <matthew.taggart@cardcash.com>
2017-12-22 23:36:21 -0500

    Add default withCredentials property on axios instance.

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,6 +5,8 @@ axios.interceptors.response.use(
   error => Promise.reject(error.response.data),
 );
 
+axios.defaults.withCredentials = true;
+
 const baseUrl = 'http://192.168.99.100:3000';
 
 export const fetchCharacters = () => axios.get(`${baseUrl}/characters`);
@@ -12,13 +14,13 @@ export const fetchComics = () => axios.get(`${baseUrl}/comics`);
 export const fetchCreators = () => axios.get(`${baseUrl}/creators`);
 export const fetchEvents = () => axios.get(`${baseUrl}/events`);
 export const fetchSeries = () => axios.get(`${baseUrl}/series`);
-export const fetchUser = () => axios.get(`${baseUrl}/user`, { withCredentials: true });
+export const fetchUser = () => axios.get(`${baseUrl}/user`);
 export const fetchSelectedCharacter = id => () => axios.get(`${baseUrl}/characters/${id}`);
 export const fetchSelectedComic = id => () => axios.get(`${baseUrl}/comics/${id}`);
 export const fetchSelectedCreator = id => () => axios.get(`${baseUrl}/creators/${id}`);
 export const fetchSelectedEvent = id => () => axios.get(`${baseUrl}/events/${id}`);
 export const fetchSelectedSeries = id => () => axios.get(`${baseUrl}/series/${id}`);
-export const saveResource = ({ resourceType, id }) => axios.post(`${baseUrl}/user/${resourceType}/${id}`, { withCredentials: true });
+export const saveResource = ({ resourceType, id }) => axios.post(`${baseUrl}/user/${resourceType}/${id}`);
 export const signIn = credentials => axios.post(`${baseUrl}/login`, { ...credentials });
 export const register = payload => axios.post(`${baseUrl}/user`,  { ...payload });
-export const logout = () => axios.delete(`${baseUrl}/logout`, { withCredentials: true });
+export const logout = () => axios.delete(`${baseUrl}/logout`);
