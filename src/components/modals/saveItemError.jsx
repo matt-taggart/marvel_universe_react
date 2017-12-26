e16e9f8ea4e6ea63cb17d1b3daf1f30be1127209
Matthew Taggart <matthew.taggart@cardcash.com>
2017-12-26 16:38:58 -0500

    Link to registration page in modal & add prop types.

--- src/components/modals/saveItemError.jsx
+++ src/components/modals/saveItemError.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import history from '../../utils/history';
 import Modal from 'react-modal';
 
-export default ({ isOpen, hideSaveItemErrorModal }) => (
-  <Modal
-    isOpen={isOpen}
-    style={{
-      content: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        height: '50vh',
-        margin: '0 auto',
-        textAlign: 'center',  
-      },
-    }}
-  >
-    <span className="close-thin" role="button" onClick={hideSaveItemErrorModal} />
-    <h1 className="is-size-4 mar-t-2">Sorry!</h1>
-    <div>
-      <p>You must be logged in to use this feature. Don't have an account yet? Click here to register.</p>
-    </div>
-  </Modal>
-)
+const SaveItemErrorModal = ({ isOpen, hideSaveItemErrorModal }) => {
+  const redirectToRegistrationPage = () => {
+    history.push('/register');
+    hideSaveItemErrorModal();
+  };
+  return (
+    <Modal
+      isOpen={isOpen}
+      style={{
+        content: {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: '25vh',
+          margin: '0 auto',
+          textAlign: 'center',
+        },
+      }}
+    >
+      <span className="close-thin" role="button" onClick={hideSaveItemErrorModal} />
+      <h1 className="is-size-4 mar-t-2">Sorry!</h1>
+      <div>
+        <p>
+          You must be logged in to use this feature. Don't have an account yet? 
+          <a role="button" onClick={redirectToRegistrationPage}> Click here to register. </a>
+        </p>
+      </div>
+    </Modal>
+  );
+};
+
+SaveItemErrorModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  hideSaveItemErrorModal: PropTypes.func.isRequired,
+};
+
+export default SaveItemErrorModal;
+
