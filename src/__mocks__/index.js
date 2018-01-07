12d09a4345447bd885e62859baef560230fb3f98
Matthew Taggart <matthew.taggart@cardcash.com>
2018-01-07 00:15:25 -0500

    Add mocks/testings for sign in saga.

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -1,15 +1,25 @@
-import { call, put, takeEvery, select } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import * as Api from '../utils/api';
 import history from '../utils/history';
 import { LOADING, FETCH_FAILED } from '../constants/display';
 import {
-  SIGN_IN_SUCCEEDED, 
-  SIGN_IN_ATTEMPT,
-  LOGOUT,
+  SIGN_IN_SUCCEEDED,
   LOGOUT_SUCCEEDED,
 } from '../constants/auth';
 
-function* signIn() {
+const mockHistory = {
+  push: jest.fn(value => value),
+};
+
+const mockSignInApiResponse = jest.fn()
+  .mockReturnValueOnce({
+    data: {
+      username: 'Pikachu',
+      email: 'pikachu@pokemon.com',
+    },
+  });
+
+export const signIn = function* signIn() {
   try {
     yield put({ type: LOADING, payload: true });
 
@@ -18,18 +28,18 @@ function* signIn() {
       password: state.form.signIn.values.password,
     }));
 
-    const user = yield call(Api.signIn, credentials);
+    const user = yield call(mockSignInApiResponse, credentials);
 
     yield put({ type: SIGN_IN_SUCCEEDED, user: user.data });
     yield put({ type: LOADING, payload: false });
-    yield call(history.push, '/profile');
+    yield call(mockHistory.push, '/profile');
   } catch (e) {
     yield put({ type: LOADING, payload: false });
     yield put({ type: FETCH_FAILED, error: e });
   }
 }
 
-function* logout() {
+export const logout = function* logout() {
   try {
     yield put({ type: LOADING, payload: true });
     yield call(Api.logout);
@@ -41,11 +51,3 @@ function* logout() {
     yield put({ type: FETCH_FAILED, error: e });
   }
 }
-
-
-function* signInAttempt() {
-  yield takeEvery(SIGN_IN_ATTEMPT, signIn);
-  yield takeEvery(LOGOUT, logout);
-}
-
-export default signInAttempt;
---
+++ src/__tests__/sagas/auth.js
@@ -0,0 +1,39 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { call, takeEvery } from 'redux-saga/effects';
+import { runSaga } from 'redux-saga';
+import { signIn, logout } from '../../__mocks__';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Auth Sagas', () => {
+  //TODO: Add negative tests
+  it('Should test sign in saga', async () => {   
+    const dispatched = [];
+
+    const result = await runSaga({
+      dispatch: action => dispatched.push(action),
+      getState: () => ({
+        form: { 
+          signIn: { 
+            values: {
+              email: 'pikachu@pokemon.com',
+              password: 'ash123'
+            } 
+          }
+        }
+      })
+    }, signIn).done;
+
+    expect(dispatched[0]).toEqual({ type: 'LOADING', payload: true });
+    expect(dispatched[1])
+      .toEqual({ 
+        type: 'SIGN_IN_SUCCEEDED', 
+        user: {
+          username: 'Pikachu',
+          email: 'pikachu@pokemon.com',
+        }
+      });
+    expect(dispatched[2]).toEqual({ type: 'LOADING', payload: false });
+  });
+});
