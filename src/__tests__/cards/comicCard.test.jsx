62e0f959aaaa5ccec44f78fe7093127064be457d
Matthew Taggart <matthew.taggart@cardcash.com>
2018-01-10 23:02:42 -0500

    Remove react dom dependency from tests.

--- src/__tests__/cards/comicCard.test.jsx
+++ src/__tests__/cards/comicCard.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ComicCard from '../../components/cards/comicCard';
